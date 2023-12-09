e9399664c9800b0f8da975802b96419dfddd2f87
Ganesh Pathan <118661019+GaneshPathan1015@users.noreply.github.com>
2023-12-09 21:56:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let year = parseInt(prompt());
+
+let ans = function isleap(year) {
+
+	if(year%4===0){
+		if(year%100===0 ){
+			return year%400===0?366:365;
+		}
+		else{
+			return 366;
+		}
+		
+	}
+	else{
+		return 365;
+	}
+	
+}
+console.log(ans);
